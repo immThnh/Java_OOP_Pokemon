b35874466cd763921c92d28b2b66f8d87ea3a53e
immThnh <103478846+immThnh@users.noreply.github.com>
2022-09-10 03:02:32 +0700

    Create Program.java

---
+++ Program.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Program {
+    public static void main(String[] args) {
+        Feature feature = new Feature();
+        // feature.addNewPokemon();
+        // feature.InputListPokemon();
+        feature.inputAvailable();
+        // feature.OutputListPokemon();
+        feature.beginBattle();
+    }
+}
